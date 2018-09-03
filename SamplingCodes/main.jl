990163f5a3841a2726ef3524265f6f6da2bff8bc
Nick Moore <nickmoore83@gmail.com>
2018-09-02 23:08:37 -0400

    NM

--- SamplingCodes/main.jl
+++ SamplingCodes/main.jl
@@ -38,8 +38,25 @@ end
 #sample_gibbs(10, 2*10^7, -0.3, 4., 1.0, "up")
 #sample_gibbs(10, 2*10^7, -0.3, 4., 0.5, "dn")
 
-function aaa()
-	microcan(nmodes,nsamples)
+function HdnGup(H3vec::Vector{Float64}, H2vec::Vector{Float64}, 
+		thup::Float64, E0::Float64, D0::Float64)
+	Hdn_mean, normconst = 0.,0.
+	for nn=1:endof(H3vec)
+		# Compute the upstream and downstream Hamiltonians.
+		Hup = sqrt(E0)*H3vec[nn] - H2vec[nn]
+		Hdn = D0^(-13/4)*sqrt(E0)*H3vec[nn] - D0^(3/2)*H2vec[nn]
+		# Compute the mean of Hdn under Gup.
+		Hdn_mean += exp(-thup*Hup) * Hdn
+		normconst += exp(-thup*Hup)
+	end
+	return Hdn_mean/normconst
+end
+
+function main()
+	H3vec, H2vec, rvar = microcan(nmodes,nsamples)
+	HdnGup()
+
+
 
 
 
