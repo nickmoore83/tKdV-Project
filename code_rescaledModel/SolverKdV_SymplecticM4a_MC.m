6725e4eed37762b29b1bedb5142a9e1df37c27d3
Nick Moore <mnmoore2@fsu.edu>
2019-09-16 08:03:10 -0500

    NM

--- code_rescaledModel/SolverKdV_SymplecticM4a_MC.m
+++ code_rescaledModel/SolverKdV_SymplecticM4a_MC.m
@@ -4,7 +4,7 @@ function SolverKdV_SymplecticM4a_MC(MC, um, Nw, theta, gibd, fi)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Set simulation parameters
-J = 16;      % Number of points in each direction, J dividable by 4
+J = 16;      % Number of points in each direction, J divisible by 4
 % MC = 1E4;  % reference MC size
 dt = .5E-3;    % initial time step size
 Nt = 1e4;    % Number of time steps
