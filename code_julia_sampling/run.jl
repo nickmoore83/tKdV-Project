418c95db5b7df067f1ec01f820fd0cdd1802942b
nickmoore83 <nickmoore83@gmail.com>
2019-10-14 18:06:29 -0400

    NM

--- code_julia_sampling/run.jl
+++ code_julia_sampling/run.jl
@@ -3,10 +3,8 @@ include("transfer.jl")
 
 
 
-
-@time( uniform_sample(16, 2) )
-
-#@time( transfun("rand-16z-1000.jld", 8) )
-
-#output_text("thvars-16z-8-1000.jld")
+# Test case with Lambda = 16, nsweeps = 2, lamfac = 8.
+#@time( uniform_sample(16, 2) )
+#@time( transfun("rand-16z-2.jld", 8) )
+#output_text("thvars-16z-8-2.jld")
 
