ae61bbbdc951d71b228b5a526d1e31e946552c1e
nickmoore83 <nickmoore83@gmail.com>
2019-10-14 17:26:45 -0400

    NM

--- code_julia_sampling/transfer.jl
+++ code_julia_sampling/transfer.jl
@@ -75,7 +75,7 @@ end
 ## RUN STUFF
 #@time( uniform_sample(16, 2, false) )
 
-#@time( transfun("rand-15-1000.jld", 8) )
+#@time( transfun("rand-16z-1000.jld", 8) )
 
-#output_text("thvars-15-8-1000.jld")
+#output_text("thvars-16z-8-1000.jld")
 
