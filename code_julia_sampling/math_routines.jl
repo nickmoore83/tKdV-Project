c5f6d2f4b4b0e7404e130a8ce3ff89504be3cbd2
Nick Moore <mnmoore2@fsu.edu>
2020-04-30 16:56:09 -0400

    NM

--- code_julia_sampling/math_routines.jl
+++ code_julia_sampling/math_routines.jl
@@ -126,8 +126,7 @@ end
 Uses several sweeps manages the memory better. 
 Note: zerolast zeroes the last mode as is done in the Matlab DNS. 
 Note: savemicro not yet addressed at all. =#
-function uniform_sample(nmodes::Int, nsweeps::Int, 
-		zerolast::Bool=true, savemicro::Bool=false)
+function uniform_sample(nmodes::Int, nsweeps::Int; zerolast::Bool=true, savemicro::Bool=false)
 	samp_per = 10^5
 	totsamp = nsweeps*samp_per
 	H2vec, H3vec = [zeros(Float32,totsamp) for nn=1:2]
@@ -154,20 +153,72 @@ function data_folder()
 	return "./Data/"
 end
 
-function writedata(data::Array, filename::AbstractString)
-	iostream = open(filename, "w")
-	writedlm(iostream, data)
-	close(iostream)
-end
-
-function readvec(file::AbstractString)
-	iostream = open(file, "r")
-	vec1 = readdlm(iostream, comments=true, comment_char='#')[:,1]
-	close(iostream)
-	return vec1
-end
 
 # Significant digits
 function sig(var, sigdig)
 	return round(var,sigdigits=sigdig)
+end
+
+
+
+
+
+
+
+
+
+
+
+#= Calculate the transfer function:
+For given upstream theta, enforce the matching condition to 
+compute the downstream theta.=#
+
+function transfun(randfile::AbstractString, lamfac::Int)
+	randfile = string(data_folder(), randfile)
+	rr, nmodes, nsweeps = load(randfile, "rr", "nmodes", "nsweeps")
+	# Parameters
+	eps0 = 0.017
+	del0 = 0.23
+	D0 = 0.24
+	# Constants
+	C2,C3 = C2C3(eps0,del0,lamfac)
+	cc = ConstantList(C2,C3,D0)
+	# Compute the list of Hamiltonians using precomputed H2 and H3
+	hdn = Float32.(hamdn(rr,cc))
+	hup = Float32.(hamup(rr,cc))
+
+	# Specify list of upstream thetas.
+	thup = 0.: 0.5 : 30.
+	nth = length(thup)
+	thdn = zeros(nth); skup = zeros(nth); skdn = zeros(nth)
+	guess = thup[1]
+	for nn = 1:nth
+		meandiff(thdn) = meanham(hdn, hdn, thdn) - meanham(hdn, hup, thup[nn])
+		thdn[nn] = find_zero(meandiff, guess, Order1())
+		guess = thdn[nn]
+		nn>1 ? guess = 2*thdn[nn] - thdn[nn-1] : 0.	# Linear extrapolation
+		println("Iteration ", nn, "; thup = ", thup[nn], "; thdn = ", sig(thdn[nn],3))
+
+		# Compute the skewness of eta
+		skup[nn] = skewu(rr.H3, hup, thup[nn])
+		skdn[nn] = skewu(rr.H3, hdn, thdn[nn])
+		println("Upstream skewness = ", sig(skup[nn],3))
+		println("Downstream skewness = ", sig(skdn[nn],3), "\n")
+	end
+	savefile = string(data_folder(), "thvars-", string(nmodes), "z-", 
+		string(lamfac), "-", string(nsweeps), ".jld")
+	save(savefile, "thup", thup, "thdn", thdn, "skup", skup, "skdn", skdn, 
+		"nmodes", nmodes, "lamfac", lamfac, "nsweeps", nsweeps)
+end
+
+function output_text(datafile::AbstractString)
+	datafile = string(data_folder(), datafile)
+	thup,thdn,skup,skdn = load(datafile, "thup", "thdn", "skup", "skdn")
+	nmodes,lamfac,nsweeps = load(datafile, "nmodes", "lamfac", "nsweeps")
+	nthvals = length(thup)
+	ratio = skdn./skup
+	outdata = [nmodes; lamfac; nthvals; thup; thdn; skup; skdn]
+	outfile = string(data_folder(), "transf-", string(nmodes), "z-", 
+		string(lamfac), "-", string(nsweeps), ".txt")
+	writedlm(outfile, outdata)
 end
